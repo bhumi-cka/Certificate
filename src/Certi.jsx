e8fac0bb3ad70044cc2fddb7abbdf2f98088702b
Bhumicka Bhaangaley <bhumickabhaangaley@gmail.com>
2023-02-02 21:20:49 +0530

    Added certificate

--- src/Certi.jsx
+++ src/Certi.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import {info} from "./Input";
 
 function Certi() {
-    return <div>
-        <h1>This is heading.</h1>
-    </div>
+    const information = info;
+    return (
+    <div>
+        <div class="main">
+        <div class="top">
+            <h1 class="heading">Certificate of Achievement</h1>
+        </div>
+        <p>This certificate is presented to</p>
+        <p class="name">{information[0].name}</p>
+        <p class="roll">Roll number: {information[0].roll}</p>
+        <p>in recognition of outstanding performance in [event name]</p>
+        <p>IIT Madras is honoured to award this certificate.</p>
+        <div class="flex">
+          <p class="date">Date:</p>
+          <p class="signature">Signature:</p>
+        </div>
+        </div>
+        </div>
+    )
 }
 
 export default Certi;
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Input() {
+const info = [];
+
+function Input() {
+
+  const [name, setName] = useState("");
+  const [roll, setRoll] = useState("");
+
+  function handleChangeName(event) {
+    setName(event.target.value);
+  }
+
+  function handleChangeRoll(event) {
+    setRoll(event.target.value);
+  }
+
+  const submitt = ()=>{
+    info.push({name : name , roll : roll});
+  }
+
   return (
   <div>
       <form>
-          <label>Name</label>
-          <input type="text" id="name" />
-          <label>Roll Number</label>
-          <input type="text" id="roll" />
+          <input 
+            type="text"
+            placeholder="Name"
+            onChange={handleChangeName}
+          />
+          <input 
+            type="text"
+            placeholder="Roll Number"
+            onChange={handleChangeRoll}
+          />
       </form>
       <Link to='/Certi'>
-      <button type="submit">Submit</button>
+      <button type="submit" onClick={submitt}>Submit</button>
       </Link>
   </div>
   )
 }
+
+export default Input;
+
+export {info};
+
+
